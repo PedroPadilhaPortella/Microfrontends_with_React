a34523eb4d614cd1e66b275916031ff644f6b672
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-10-18 00:03:03 -0300

    feat: Solving Problems related to MFE routing beetwen projects

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,10 +1,11 @@
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
-import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+import AuthApp from './components/AuthApp';
 
-const generateClassName = createGenerateClassName({ productionPrefix: 'ct' })
+const generateClassName = createGenerateClassName({ productionPrefix: 'co' })
 
 export default () => {
   return (
@@ -12,7 +13,10 @@ export default () => {
       <StylesProvider generateClassName={generateClassName} >
         <div>
           <Header />
-          <MarketingApp />
+          <Switch>
+            <Route path="/auth" component={AuthApp} />
+            <Route path="/" component={MarketingApp} />
+          </Switch>
         </div>
       </StylesProvider>
     </BrowserRouter>
